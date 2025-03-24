4cf5aa521fb102c20ab69627e73d5d508cc301bb
KushalChandani2 <kc07535@st.habib.edu.pk>
2025-03-24 22:54:48 +0500

    Project 4

---
+++ CI-CD Pipeline for Flask-Based Portfolio Deployment Using GitHub Actions, Docker, and Kubernetes/portfolio/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, send_from_directory
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html", title="Portfolio - Home")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="Portfolio - About")
+
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html", title="Portfolio - Projects")
+
+
+@app.route("/static/<path:path>")
+def send_static(path):
+    return send_from_directory("static", path)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
